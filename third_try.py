b7c1cba2db4454ddf2b9915604837dfeff6e41a5
Jefferson Stachelski <jeffhsta@riseup.net>
2015-07-08 18:44:02 -0300

    Added the HMAC value to the end cipher text result

--- third_try.py
+++ third_try.py
@@ -1,12 +1,15 @@
 import os
 import hmac
+from hashlib import sha256
 from pycryptopp.cipher.aes import AES
 
 plain_text = "Text to be encrypted"
 secret_key = "* super secret key of 32 bytes *"
 iv = os.urandom(16)
+
 cipher_text = AES(key=secret_key, iv=iv).process(plain_text)
-<hmac example here>
+text_hmac = hmac.HMAC(key, ''.join(iv, cipher_text), sha256)
+cipher_text = ''.join(text_hmac, cipher_text)
 
 with open('file') as file:
   file.write(cipher_text)
