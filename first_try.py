feac6213e94eb62c2a2dc95f59c56d3c4d1c9e7e
Jefferson Stachelski <jeffhsta@riseup.net>
2015-07-10 11:47:42 -0300

    Fixed the open file without 'w' option

--- first_try.py
+++ first_try.py
@@ -4,5 +4,5 @@ plain_text = "Text to be encrypted"
 secret_key = "* super secret key of 32 bytes *"
 cipher_text = AES(key=secret_key).process(plain_text)
 
-with open('file') as file:
+with open('file1', 'w') as file:
   file.write(cipher_text)
--- second_try.py
+++ second_try.py
@@ -6,5 +6,5 @@ secret_key = "* super secret key of 32 bytes *"
 iv = os.urandom(16)
 cipher_text = AES(key=secret_key, iv=iv).process(plain_text)
 
-with open('file') as file:
+with open('file2', 'w') as file:
   file.write(cipher_text)
