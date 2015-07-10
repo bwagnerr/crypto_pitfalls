c1a436b183127473d4864b18f8a21b65e2947603
Jefferson Stachelski <jeffhsta@riseup.net>
2015-07-10 11:47:15 -0300

    Fixed some bug using string join

--- third_try.py
+++ third_try.py
@@ -8,8 +8,8 @@ secret_key = "* super secret key of 32 bytes *"
 iv = os.urandom(16)
 
 cipher_text = AES(key=secret_key, iv=iv).process(plain_text)
-text_hmac = hmac.HMAC(key, ''.join(iv, cipher_text), sha256)
-cipher_text = ''.join(text_hmac, cipher_text)
+text_hmac = hmac.new(secret_key, ''.join((iv, cipher_text)), sha256).digest()
+cipher_text = ''.join((text_hmac, cipher_text))
 
-with open('file') as file:
+with open('file3', 'w') as file:
   file.write(cipher_text)
